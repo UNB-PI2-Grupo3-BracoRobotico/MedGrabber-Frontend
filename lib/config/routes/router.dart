0e1683ed8ea00046d310e8616d8aea5f6465fbf3
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2023-06-04 18:03:17 -0300

    refac: change routes folder location

--- lib/config/routes/router.dart
+++ lib/config/routes/router.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:grabber/config/routes.dart';
+import 'package:grabber/config/routes/routes.dart';
 import 'package:grabber/features/navigation/nav_screen_1.dart';
 import 'package:grabber/features/navigation/nav_screen_2.dart';
 
