bda58bdcdc7dc2f8029dab88b9c8f3fb9e042f60
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2023-06-06 22:58:32 -0300

    refac: cahnge placemente of steps pages

--- lib/features/setup_machine/presentation/pages/step_1.dart
+++ lib/features/setup_machine/presentation/pages/step_1.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:grabber/features/setup_machine/presentation/widgets/base_step_page.dart';
+import 'package:grabber/config/routes/routes.dart';
+import 'package:grabber/features/setup_machine/presentation/pages/widgets/base_step_page.dart';
 
-import '../../../generated/l10n.dart';
+import '../../../../generated/l10n.dart';
 
 class Step1 extends StatelessWidget {
   const Step1({super.key});
@@ -13,7 +14,9 @@ class Step1 extends StatelessWidget {
           "Lorem ipsum dolor sit amet consectetur. Dolor dictum nunc sed purus.",
       description:
           "Lorem ipsum dolor sit amet consectetur. Auctor aliquet scelerisque leo ut enim. Eget ornare tincidunt eu viverra in justo. Ac diam ultricies blandit pellentesque nisl enim faucibus arcu a. Sed faucibus volutpat sed cum a in non at. Urna dolor orci tempus massa. Purus semper purus eros congue eu nisi. Volutpat bibendum odio mattis tempor.",
-      onPressed: () {},
+      onPressed: () => Navigator.of(context).pushNamed(
+        AppRoutes.setup2,
+      ),
       buttonLabel: S.current.continue_button_label,
     );
   }
