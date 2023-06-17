0fdebea3c2d2e3d04a6f4197e3b67ae161372e15
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2023-06-17 19:50:29 -0300

    feat: create name page cubit

---
+++ lib/features/settings/pages/name_option/blocs/name_page/name_page_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:grabber/features/settings/domain/usecases/update_store_name.dart';
+
+part 'name_page_cubit.freezed.dart';
+part 'name_page_state.dart';
+
+class NamePageCubit extends Cubit<NamePageState> {
+  final UpdateStoreName _updateStoreName;
+  NamePageCubit({
+    required UpdateStoreName updateStoreName,
+  })  : _updateStoreName = updateStoreName,
+        super(const NamePageState.initial());
+
+  Future<void> updateStoreName(String newStoreName) async {
+    emit(const NamePageState.loading());
+    final sucessOrFailureUpdate = await _updateStoreName.call(newStoreName);
+    emit(sucessOrFailureUpdate.fold(
+      NamePageState.success,
+      (_) => const NamePageState.error(),
+    ));
+  }
+
+  Future<void> reset() async {
+    emit(const NamePageState.initial());
+  }
+}
