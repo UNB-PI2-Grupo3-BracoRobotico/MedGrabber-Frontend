ea78d8a3a24d07b7dd4df661bf28c5947591bf8e
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2023-06-19 23:58:44 -0300

    feat: add new usecase to update phone

--- lib/core/injection.config.dart
+++ lib/core/injection.config.dart
@@ -10,8 +10,10 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:grabber/features/settings/domain/usecases/update_store_name.dart'
+import 'package:grabber/features/settings/domain/usecases/update_phone_number.dart'
     as _i3;
+import 'package:grabber/features/settings/domain/usecases/update_store_name.dart'
+    as _i4;
 import 'package:injectable/injectable.dart' as _i2;
 
 extension GetItInjectableX on _i1.GetIt {
@@ -25,7 +27,8 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.factory<_i3.UpdateStoreName>(() => _i3.UpdateStoreNameImpl());
+    gh.factory<_i3.UpdatePhoneNumber>(() => _i3.UpdatePhoneNumberImpl());
+    gh.factory<_i4.UpdateStoreName>(() => _i4.UpdateStoreNameImpl());
     return this;
   }
 }
