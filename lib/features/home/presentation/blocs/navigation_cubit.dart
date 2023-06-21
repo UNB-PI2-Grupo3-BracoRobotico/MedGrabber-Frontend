db54bb00218e26d666ef12fe233779761b11f79f
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2023-06-21 00:20:03 -0300

    feat: create navigation cubit and wrap at main

---
+++ lib/features/home/presentation/blocs/navigation_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'navigation_cubit.freezed.dart';
+part 'navigation_state.dart';
+
+class NavigationCubit extends Cubit<NavigationState> {
+  NavigationCubit()
+      : super(
+          NavigationState(
+            currentIndex: 2,
+          ),
+        );
+
+  void changeIndex(int index) {
+    emit(
+      state.copyWith(
+        currentIndex: index,
+      ),
+    );
+  }
+}
