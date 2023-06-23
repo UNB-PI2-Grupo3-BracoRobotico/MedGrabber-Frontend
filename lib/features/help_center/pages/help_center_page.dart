821c2b92d4c8278210f2959bf15400d3c7ac8eb2
DaviMatheus <matdavimat@gmail.com>
2023-06-23 03:23:36 -0300

    feat: fixing_help_center_page

--- lib/features/help_center/pages/help_center_page.dart
+++ lib/features/help_center/pages/help_center_page.dart
@@ -1,61 +1,40 @@
+import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:grabber/features/shared/bottom_navigation_bar.dart';
-
 class HelpCenter extends StatelessWidget {
   const HelpCenter({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: GrabberBottomNavigationBar(),
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    bottomNavigationBar: const GrabberBottomNavigationBar(),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 16.0,
-                left: 16.0,
-              ),
-              child: Text(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: kSpacingXS,
+          ),
+          child: Column(
+            children: [
+              const VerticalGap.xl(),
+              Text(
                 'Central de Ajuda',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
+                style: Theme.of(context).textTheme.titleLarge,
               ),
-            ),
-            Expanded(
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        '../../assets/images/1.5x/headphone.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Como ajudar?',
-                        style: TextStyle(
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                const Image(
+                image: AssetImage('assets/images/headphone.png'),
+                width: 100,
+                height: 100,
               ),
-            ),
-            Expanded(
-              child: Center(
-                child: Text('HELP'),
+              const VerticalGap.xl(),
+              Text(
+                'description',
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.justify,
               ),
-            ),
-          ],
+              const Spacer(),
+              const VerticalGap.xxxs(),
+            ],
+          ),
         ),
       ),
     );
