31f7b0982972212be1fa1e7509428d15029c148a
DaviMatheus <matdavimat@gmail.com>
2023-06-24 01:44:38 -0300

    feature: change help_page

--- lib/features/help_center/pages/help_center_page.dart
+++ lib/features/help_center/pages/help_center_page.dart
@@ -1,41 +1,59 @@
+
 import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:grabber/features/shared/bottom_navigation_bar.dart';
+
+import '../../../config/routes/routes.dart';
+import '../../../generated/l10n.dart';
+import '../../help_center/widgets/real_time.dart';
+
 class HelpCenter extends StatelessWidget {
   const HelpCenter({Key? key}) : super(key: key);
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    bottomNavigationBar: const GrabberBottomNavigationBar(),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: kSpacingXS,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: const GrabberBottomNavigationBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const VerticalGap.xl(),
+          Padding(
+            padding: const EdgeInsets.only(left: 28.0),
+            child: Text(
+              'Central de Ajuda',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
           ),
-          child: Column(
-            children: [
-              const VerticalGap.xl(),
-              Text(
-                'Central de Ajuda',
-                style: Theme.of(context).textTheme.titleLarge,
+          const SizedBox(height: 60),
+          Center(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/headphone.png',
+                  width: 64,
+                  height: 64,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Como podemos ajudar',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: TextAlign.justify,
+                ),
+                const SizedBox(height: 7),
+                RealTime(
+                title: S.current.settings_name_option_title,
+                icon: Icons.account_circle_outlined,
+                onTap: () => Navigator.of(context).pushNamed(
+                  AppRoutes.settingsName,
+                ),
               ),
-                const Image(
-                image: AssetImage('assets/images/headphone.png'),
-                width: 100,
-                height: 100,
-              ),
-              const VerticalGap.xl(),
-              Text(
-                'description',
-                style: Theme.of(context).textTheme.bodyMedium,
-                textAlign: TextAlign.justify,
-              ),
-              const Spacer(),
-              const VerticalGap.xxxs(),
-            ],
+              ],
+            ),
           ),
-        ),
+          const Spacer(),
+          const VerticalGap.xxxs(),
+        ],
       ),
     );
   }
