739702080a825e7dfffccdcc269824f81c8eebc3
DaviMatheus <matdavimat@gmail.com>
2023-07-05 17:01:26 -0300

    Dashboard_page_information

--- lib/features/dashboard/presentation/pages/dashboard_page.dart
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:grabber/config/routes/routes.dart';
 import 'package:grabber/core/injection.dart';
+import 'package:grabber/features/dashboard/presentation/widgets/dashboard_information.dart';
 import 'package:grabber/features/home/presentation/widgets/informational_card.dart';
 import 'package:grabber/features/home/presentation/widgets/low_storage_items_section.dart';
 import 'package:grabber/features/inventory/presentation/blocs/inventory/has_item_cubit.dart';
@@ -11,6 +12,8 @@ import 'package:grabber/features/orders/presentation/blocs/get_orders/get_orders
 import 'package:grabber/features/orders/presentation/order_section.dart';
 import 'package:grabber/features/shared/bottom_navigation_bar.dart';
 import 'package:grabber/generated/l10n.dart';
+
+import '../../../inventory/domain/entities/product.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -53,19 +56,173 @@ class _DashboardPageState extends State<DashboardPage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: kSpacingXS),
-          
           child: Column(
             children: [
-              const VerticalGap.xl(),
+              const VerticalGap.sm(),
               DSButton.primary(
                 onPressed: () {},
                 label: S.current.dashboard_section_reports_button_label,
               ),
-              const VerticalGap.xxxs(),
+              const VerticalGap.xxl(),
+              MultiBlocProvider(
+                providers: [
+                  BlocProvider.value(
+                    value: _hasItemCubit,
+                  ),
+                  BlocProvider.value(
+                    value: _positionsAvailableCubit,
+                  ),
+                ],
+                child: const _FirstRowInformationCards(),
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+
+class _FirstRowInformationCards extends StatefulWidget {
+  const _FirstRowInformationCards();
+
+  @override
+  State<_FirstRowInformationCards> createState() =>
+      _FirstRowInformationCardsState();
+}
+
+class _FirstRowInformationCardsState extends State<_FirstRowInformationCards> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: kSpacingXS,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: BlocBuilder<PositionsAvailableCubit, PositionsAvailableState>(
+                  builder: (context, state) {
+                    return state.maybeWhen(
+                      loading: () => const DashboardCard(
+                        message: '',
+                        informationValue: 0,
+                        isLoading: true,
+                      ),
+                      error: () => DashboardCard(
+                        message: S.current.informational_card_error_message,
+                        onTap: () => context.read<PositionsAvailableCubit>().checkAvailablePositions(),
+                      ),
+                      orElse: () => DashboardCard(
+                        message: S.current.home_available_positions_card,
+                        informationValue: state.maybeWhen(
+                          orElse: () => 0,
+                          availablePositions: (positionOptions) => positionOptions.length,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const HorizontalGap.xxxs(),
+              Expanded(
+                child: BlocBuilder<HasItemCubit, HasItemState>(
+                  builder: (context, state) {
+                    return state.maybeWhen(
+                      loading: () => const DashboardCard(
+                        message: '',
+                        informationValue: 0,
+                        isLoading: true,
+                      ),
+                      error: () => DashboardCard(
+                        message: S.current.informational_card_error_message,
+                        onTap: () => context.read<HasItemCubit>().hasItemRegistered(),
+                      ),
+                      orElse: () => DashboardCard(
+                        message: S.current.home_available_itens_card,
+                        informationValue: state.maybeWhen(
+                          orElse: () => 0,
+                          hasItemRegisters: (products) => _getLowAmountItens(products).length,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+          const VerticalGap.xxxs(),
+             Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: BlocBuilder<PositionsAvailableCubit, PositionsAvailableState>(
+                  builder: (context, state) {
+                    return state.maybeWhen(
+                      loading: () => const DashboardCard(
+                        message: '',
+                        informationValue: 0,
+                        isLoading: true,
+                      ),
+                      error: () => DashboardCard(
+                        message: S.current.informational_card_error_message,
+                        onTap: () => context.read<PositionsAvailableCubit>().checkAvailablePositions(),
+                      ),
+                      orElse: () => DashboardCard(
+                        message: S.current.home_available_positions_card,
+                        informationValue: state.maybeWhen(
+                          orElse: () => 0,
+                          availablePositions: (positionOptions) => positionOptions.length,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const HorizontalGap.xxxs(),
+              Expanded(
+                child: BlocBuilder<HasItemCubit, HasItemState>(
+                  builder: (context, state) {
+                    return state.maybeWhen(
+                      loading: () => const DashboardCard(
+                        message: '',
+                        informationValue: 0,
+                        isLoading: true,
+                      ),
+                      error: () => DashboardCard(
+                        message: S.current.informational_card_error_message,
+                        onTap: () => context.read<HasItemCubit>().hasItemRegistered(),
+                      ),
+                      orElse: () => DashboardCard(
+                        message: S.current.home_available_itens_card,
+                        informationValue: state.maybeWhen(
+                          orElse: () => 0,
+                          hasItemRegisters: (products) => _getLowAmountItens(products).length,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Product> _getLowAmountItens(List<Product> products) {
+    List<Product> productsEnding = [];
+    for (final product in products) {
+      if (product.amount < 2) {
+        productsEnding.add(product);
+      }
+    }
+    return productsEnding;
   }
 }
